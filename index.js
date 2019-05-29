c78da8ef69ab9d5d734286ba7a1e3ae1a42a56b5
kaichiro <kcfg23@gmail.com>
2019-05-29 14:42:48 -0300

    Refactoring/remove route labels and clear console log messages

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const mysql = require('mysql2')
 require('dotenv/config')
 
-const {
-    customersGetAll,
-    customerById,
-    customersLikeName,
-    customerDeleteById,
-    customerCreate,
-    customerUpdate,
-} = require('./routes/index')
-
 const app_port = process.env.APP_PORT || 3000
 const app_address = `http://localhost`
 const connection = mysql.createConnection({
@@ -28,16 +19,6 @@ app.listen(app_port, (err) => {
         console.log(`App doesn't work: `, err)
     } else {
         const addressBase = `${app_address}:${app_port}`
-        console.log(`API works!`)
-        console.log('Method: GET')
-        console.log(`  all       = ${addressBase}${customersGetAll}`)
-        console.log(`  by Id     = ${addressBase}${customerById.replace(':id', '51073bb0-7ff1-11e9-a38f-18037356addd')}`)
-        console.log(`  like Name = ${addressBase}${customersLikeName.replace(':name', 'Kaichiro')}`)
-        console.log('Method: DELETE')
-        console.log(`  by Id     = ${addressBase}${customerDeleteById.replace(':id', '51073bb0-7ff1-11e9-a38f-18037356addd')}`)
-        console.log('Method: POST')
-        console.log(`  create    = ${addressBase}${customerCreate}`)
-        console.log('Method: PUT')
-        console.log(`  update    = ${addressBase}${customerUpdate}`)
+        console.log(`API works! ${addressBase}`)
     }
 })
